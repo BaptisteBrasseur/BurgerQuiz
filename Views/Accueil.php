9585dbadede028b01df35ddcec4ac54224f6dba2
Ardui Thomas <ardui.thomas@gmail.com>
2024-03-27 17:05:16 +0100

    Style page aaccueil début;

--- Views/Accueil.php
+++ Views/Accueil.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil-Burger Quizz </title>
-    <link rel="stylesheet" href="../Styles/accueil.css">
+    <link rel="stylesheet" href="../Style/style.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
@@ -29,24 +29,33 @@ include "../Includes/header.php";
      dnaissance: birth
      nom utilisateur: username
      mot de passe : password-->
+    <div class="row">
+        <div class="col-12 text-center">
+            <img src="../images/imageaccueil.jpg" alt="" class="img-fluid rounded" >
+        </div>
+    </div>
 
-    <h2> Connectez-vous </h2>
+    <div class="alignementformulaires">
 
 
-    <form method="post" action="">
+        <div class="formconnection">
+            <h2> Connectez-vous </h2>
+            <form method="post" action="">
 
-        <p>
-            <label for="nuserconnection"> Nom d'utilisateur: </label>
-            <input type="text" name="username"  id="nuserconnection" required>
-        </p>
-        <p>
-            <label for="mpasse">Mot de passe: </label>
-            <input type="password" name="password"  id="mpasse" required>
-        </p>
-        <input type="submit"  value="Connexion">
-    </form>
+                <p>
+                    <label for="nuserconnection"> Nom d'utilisateur: </label>
+                    <input type="text" name="username"  id="nuserconnection" required>
+                </p>
+                <p>
+                    <label for="mpasse">Mot de passe: </label>
+                    <input type="password" name="password"  id="mpasse" required>
+                </p>
+                <input type="submit"  value="Connexion">
+            </form>
+            </div>
 
-    <h2> Inscrivez-vous </h2>
+
+<div class="formregister"> <h2> Inscrivez-vous</h2>
     <form method="post" action="">
 
 
@@ -79,10 +88,12 @@ include "../Includes/header.php";
         </p>
         <input type="submit"  value="S'inscrire">
     </form>
+</div>
 
 
 
 </div>
+
 
 
 
