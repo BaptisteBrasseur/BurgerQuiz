f0293612d7bec196ee1c2442908278a8753f85c1
unknown <baptistebrasseur@yahoo.fr>
2024-03-27 13:44:30 +0100

    accueil de base

---
+++ Views/Accueil.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr-BE">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="../Styles/accueil.css">
+</head>
+<body>
+<?php
+include "../Includes/header.php";
+?>
+
+</body>
+<?php
+include "../Includes/footer.php";
+?>
+</html>
