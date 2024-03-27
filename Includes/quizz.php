9ae52af8054e5edda5df6a9c40fed77b83fc3e76
unknown <baptistebrasseur@yahoo.fr>
2024-03-27 15:29:04 +0100

    quizz à jour

--- Includes/quizz.php
+++ Includes/quizz.php
@@ -1,45 +1,18 @@
 <?php
 declare(strict_types=1);
-do {
-    $compteur=0;
-    echo "Quel est votre nom?";
-    $nom=trim(fgets(STDIN));
-    echo "Bonjour ",$nom,". Vous allez jouer à un jeu de questions-réponses. Tapez 1 pour choisir les mangas, tapez 2 pour choisir la littérature ou tapez 3 pour quitter.\n";
-    $nbr=trim(fgets(STDIN));
-    if ($nbr==1)
-    {
-        $compteur=+Question("Question 1: Lequel de ces mages peut manger du feu?\nTapez a pour 'Natsu Dragneel', tapez b pour 'Lucy Heartfillia' ou tapez c pour 'Erza Scarlet'.\n","a",$compteur);
-        $compteur=+Question("Question 2: Que porte Luffy sur sa tête?\nTapez a pour 'un chapeau de pirate', tapez b pour 'un chapeau de paille' ou tapez c pour 'un chapeau de cowboy'.\n","b",$compteur);
-        $compteur=+Question("Question 3: Comment s'appelle 'Ken le survivant' en japonais?\nTapez a pour 'Ansatsu Kyoshitsu', tapez b pour 'Rattata to ta' ou tapez c pour 'Hokuto no Ken'.\n","c",$compteur);
-        $compteur=+Question("Question 4: Quelle est l'arme d'Ichigo dans 'Bleach''?\nTapez a pour 'une hache', tapez b pour 'une épée' ou tapez c pour 'une lance'.\n","b",$compteur);
+$compteur=0;
+        $compteur=+Question("Question 1: Quel mangaka a écrit 'Astro Boy'?\nTapez a pour 'Osamu Tezuka', tapez b pour 'Akira Toriyama' ou tapez c pour 'Go Nagai'.\n","a",$compteur);
+        $compteur=+Question("Question 2: Que porte Monkey D. Luffy sur sa tête?\nTapez a pour 'un chapeau de pirate', tapez b pour 'un chapeau de paille' ou tapez c pour 'un chapeau de cowboy'.\n","b",$compteur);
+        $compteur=+Question("Question 3: Quel studio a adapté en anime 'Chainsaw Man'?\nTapez a pour 'Toei Animation', tapez b pour 'Studio Pierrot' ou tapez c pour 'MAPPA'.\n","c",$compteur);
+        $compteur=+Question("Question 4: Lequel de ces mangas n'est pas paru dans le 'Weekly Shonen Jump'?\nTapez a pour 'Bleach', tapez b pour 'L'attaque des Titans' ou tapez c pour 'Death Note'.\n","b",$compteur);
         $compteur=+Question("Question 5: En quoi se transforme Ranma en touchant de l'eau froide?\nTapez a pour 'en fille', tapez b pour 'en panda' ou tapez c pour 'en écrevisse'.\n","a",$compteur);
         echo "Maintenant vous devrez directement tapez la bonne réponse.\n";
-        $compteur=+Question("Question 6: Quel est le signe du zodiaque de Seiya?\n","sagittaire",$compteur);
-        $compteur=+Question("Question 7: Combien y a-t-il de Dragon Balls sur Namek en chiffres?\n","7",$compteur);
+        $compteur=+Question("Question 6: Quel est le prénom de l'héroïne de 'La rose de Versailles'?\n","oscar",$compteur);
+        $compteur=+Question("Question 7: En quelle année s'est terminé le manga 'Dragon Ball' en chiffres?\n","1995",$compteur);
         $compteur=+Question("Question 8: Quel est le nom de famille de Naruto?\n","uzumaki",$compteur);
-        $compteur=+Question("Question 9: Completer le nom de ce manga: '______ chasseuse de cartes'?\n","sakura",$compteur);
+        $compteur=+Question("Question 9: Quel est le collectif derrière 'Sakura chasseuse de cartes'?\n","clamp",$compteur);
         $compteur=+Question("Question 10: Quel est le métier des frères Elric?\n","alchimiste",$compteur);
         echo "Vous avez trouvé ",$compteur," bonne réponses. Bravo!\n";
-        echo "Tapez 'q' pour quitter ou toute autre touche pour recommencer.\n";
-        $suite=trim(fgets(STDIN));
-        if ($suite=="q")
-        {
-            break;
-        }
-    }
-    elseif ($nbr==2)
-    {
-        $compteur=+Question("Question 1: Qui a écrit ''?\nTapez a pour 'San', tapez b pour 'Beau' ou tapez c pour 'Gohan'.\n","a",$compteur);
-    }
-    elseif ($nbr==3)
-    {
-        break;
-    }
-    else
-    {
-        echo "La commande est erronée, veuillez réessayer.\n";
-    }
-}while ($nbr!=3);
 
 function Question(string $_question, string $_bon, int $_compteur): int
 {
