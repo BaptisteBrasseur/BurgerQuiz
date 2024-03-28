97db0c9d45d9a3776b9479d2198746e8ed0df7e1
unknown <baptistebrasseur@yahoo.fr>
2024-03-28 09:59:11 +0100

    quizz 2.0

--- Includes/quizz.php
+++ Includes/quizz.php
@@ -1,11 +1,11 @@
 <?php
 declare(strict_types=1);
 $compteur=0;
-        $compteur=+Question("Question 1: Quel mangaka a écrit 'Astro Boy'?\nTapez a pour 'Osamu Tezuka', tapez b pour 'Akira Toriyama' ou tapez c pour 'Go Nagai'.\n","a",$compteur);
-        $compteur=+Question("Question 2: Que porte Monkey D. Luffy sur sa tête?\nTapez a pour 'un chapeau de pirate', tapez b pour 'un chapeau de paille' ou tapez c pour 'un chapeau de cowboy'.\n","b",$compteur);
-        $compteur=+Question("Question 3: Quel studio a adapté en anime 'Chainsaw Man'?\nTapez a pour 'Toei Animation', tapez b pour 'Studio Pierrot' ou tapez c pour 'MAPPA'.\n","c",$compteur);
-        $compteur=+Question("Question 4: Lequel de ces mangas n'est pas paru dans le 'Weekly Shonen Jump'?\nTapez a pour 'Bleach', tapez b pour 'L'attaque des Titans' ou tapez c pour 'Death Note'.\n","b",$compteur);
-        $compteur=+Question("Question 5: En quoi se transforme Ranma en touchant de l'eau froide?\nTapez a pour 'en fille', tapez b pour 'en panda' ou tapez c pour 'en écrevisse'.\n","a",$compteur);
+        $compteur=+Question("Question 1: Quel mangaka a écrit 'Astro Boy'?\nA 'Osamu Tezuka', B 'Akira Toriyama' ou C 'Go Nagai'.\n","a",$compteur);
+        $compteur=+Question("Question 2: Que porte Monkey D. Luffy sur sa tête?\nA 'un chapeau de pirate', B 'un chapeau de paille' ou C 'un chapeau de cowboy'.\n","b",$compteur);
+        $compteur=+Question("Question 3: Quel studio a adapté en anime 'Chainsaw Man'?\nA 'Toei Animation', B 'Studio Pierrot' ou C 'MAPPA'.\n","c",$compteur);
+        $compteur=+Question("Question 4: Lequel de ces mangas n'est pas paru dans le 'Weekly Shonen Jump'?\nA 'Bleach', B 'L'attaque des Titans' ou C 'Death Note'.\n","b",$compteur);
+        $compteur=+Question("Question 5: En quoi se transforme Ranma en touchant de l'eau froide?\nA 'en fille', B 'en panda' ou C 'en écrevisse'.\n","a",$compteur);
         echo "Maintenant vous devrez directement tapez la bonne réponse.\n";
         $compteur=+Question("Question 6: Quel est le prénom de l'héroïne de 'La rose de Versailles'?\n","oscar",$compteur);
         $compteur=+Question("Question 7: En quelle année s'est terminé le manga 'Dragon Ball' en chiffres?\n","1995",$compteur);
@@ -21,12 +21,7 @@ function Question(string $_question, string $_bon, int $_compteur): int
     $_reponse=strtolower($_reponse);
     if ($_reponse==$_bon)
     {
-        echo "Bonne réponse!\n";
         $_compteur++;
-    }
-    else
-    {
-        echo "Mauvaise réponse.\n";
     }
     return $_compteur;
 }
